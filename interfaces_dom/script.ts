8bbbc0431a2dee2a66e90c55893fa637557adbf5
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-03-19 15:09:27 -0300

    intarface dom 01

---
+++ interfaces_dom/script.ts
@@ -0,0 +1,9 @@
+// caso seja passado pelo id ou classe, 
+// o typescript nao tem como identificar o tipo real do html
+
+const video = document.querySelector('#videoEl')
+const audio = document.querySelector('audio')
+
+console.dir(video)
+console.dir(audio)
+
