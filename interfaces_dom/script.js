11c657fdafc589a148c653467866cb1d991fbe09
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-03-20 19:34:30 -0300

    interfaces dom

--- interfaces_dom/script.js
+++ interfaces_dom/script.js
@@ -3,5 +3,18 @@
 // o typescript nao tem como identificar o tipo real do html
 const video = document.querySelector('#videoEl');
 const audio = document.querySelector('audio');
+const links = document.querySelectorAll('.link');
 console.dir(video);
 console.dir(audio);
+// verificando se o item da lista e um element
+links.forEach((link) => {
+    if (link instanceof HTMLAnchorElement) {
+        console.log(link.href);
+    }
+});
+// o nodelist nao tem todos os metodos do array
+// para transfomr um nodelist em array 
+const novosLinks = Array.from(links);
+// dessa forma conseguimos pegar os links separados
+const anchorList = novosLinks.filter((link) => link instanceof HTMLAnchorElement);
+console.log(anchorList);
--- interfaces_dom/script.ts
+++ interfaces_dom/script.ts
@@ -3,7 +3,23 @@
 
 const video = document.querySelector('#videoEl')
 const audio = document.querySelector('audio')
-
+const links = document.querySelectorAll('.link')
 console.dir(video)
 console.dir(audio)
 
+// verificando se o item da lista e um element
+links.forEach((link) => {
+    if (link instanceof HTMLAnchorElement) {
+        
+        console.log(link.href)
+
+    }
+})
+// o nodelist nao tem todos os metodos do array
+
+// para transfomr um nodelist em array 
+const novosLinks = Array.from(links)
+// dessa forma conseguimos pegar os links separados
+const anchorList = novosLinks.filter((link) => link instanceof HTMLAnchorElement); 
+
+console.log(anchorList)
