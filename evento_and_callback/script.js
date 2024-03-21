bb87dd60eecbb079d1648653e7c90a350d71b19f
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-03-21 20:28:15 -0300

    event and callback

--- evento_and_callback/script.js
+++ evento_and_callback/script.js
@@ -1,17 +1,13 @@
-const button = document.querySelector('button')
-
+"use strict";
+const button = document.querySelector('button');
 // function handleClick(event: MouseEvent) {
 //     console.log(event)
 // }
-
 // button?.addEventListener("pointerdown", handleClick)
-
 // function handleScroll(event: Event) {
 //     console.log(event)
 // }
-
 // // window.addEventListener("scroll", handleScroll)
-
 // function activeMenu(e: Event) {
 //     if (e instanceof MouseEvent) {
 //         console.log(e.pageX)
@@ -21,11 +17,8 @@ const button = document.querySelector('button')
 //     }
 //     console.log(e)
 // }
-
 // document.documentElement.addEventListener("mousedown", activeMenu)
-
 // document.documentElement.addEventListener('touchstart', activeMenu)
-
 // usando o this
 // para utilizar o this é preciso passar ele como primeiro argumento da função
 // e usar o tipo de elemento 
@@ -33,18 +26,15 @@ const button = document.querySelector('button')
 // dessa forma ele retorna o elemento 
 // para verificar e acessar as propriedade e necessario fazer uma validação
 // com o instanceof
-
 // function handleClick(this: HTMLButtonElement, e: MouseEvent) {
 //     console.log(this)
 // }
-
-
-function handleClick(e: MouseEvent) {
-    const element = e.currentTarget
+function handleClick(e) {
+    const element = e.currentTarget;
     if (element instanceof HTMLElement) {
-        console.log(element.innerText)
-        element.innerHTML = "Clicou"
+        console.log(element.innerText);
+        element.innerHTML = "Clicou";
     }
-    console.log()
+    console.log();
 }
-button?.addEventListener('click', handleClick)
+button === null || button === void 0 ? void 0 : button.addEventListener('click', handleClick);
